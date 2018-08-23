66e1a68973e5c5ff0a4550a18831ac1c89ff3ca0
omochi2213 <omochikunn@gmail.com>
2018-08-23 17:52:53 +0900

    8/23

---
+++ TestProject01/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Hello Worldの出力
+		int i=10;
+		do{
+			System.out.println(i);
+			i-=3;
+		}while(i>=1);
+}
+}
---
+++ android/src/jp/co/internous/action/Android.java
@@ -0,0 +1,18 @@
+package jp.co.internous.action;
+
+public class Android {
+	public static void main(String[]args){
+		SmartPhone a =new SmartPhone();
+		a.play();
+		a.stop();
+		a.next();
+		a.back();
+		a.call();
+		a.photo();
+		a.mail();
+		a.internet();
+		a.game();
+		
+	}
+
+}
